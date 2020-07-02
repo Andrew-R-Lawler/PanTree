84568491686bb723202741f42d2812454647a447
Andrew Lawler <Andrew.R.Lawler@gmail.com>
2020-07-02 13:47:07 -0500

    updated pantreeItemSaga to include a catch on the addItem function

--- src/redux/sagas/pantreeItemSaga.js
+++ src/redux/sagas/pantreeItemSaga.js
@@ -2,9 +2,17 @@ import { put, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
 
 function* addItem (action) {
-    console.log(action.payload);
-    yield axios.post('/api/pantree', action.payload)
-    yield put ({ type: 'GET_PANTREE_ITEMS' })
+    try {
+        yield axios.post('/api/pantree', action.payload)
+        yield put ({ type: 'GET_PANTREE_ITEMS' })
+    }
+    catch (error) {
+        console.log('FAILED POST:', error);
+    }
+}
+
+function* getItem (){
+
 }
 
 function* pantreeItemSaga() {
